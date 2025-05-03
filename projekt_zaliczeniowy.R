081f6dfeeed587f203f096295e6f08788be40b5f
ola-luszczak <am.luszczak@student.uw.edu.pl>
2025-05-03 13:29:45 +0200

    Update projekt_zaliczeniowy.R

---
+++ projekt_zaliczeniowy.R
@@ -0,0 +1,15 @@
+# cel projektu:
+#stworzenie działającego skryptu w języku R, który analizuje dane tekstowe (z jednego
+#lub wielu plików .txt, .csv) 
+#przy pomocy algorytmów używanych podczas zajęć zidentyfikowanie i zbadanie trendów w danych tekstowych 
+#(np. eksploracja częstości słów, analiza sentymentu, klastrowanie, modelowanie tematów itp.)
+
+# WYMAGANIA TECHNICZNE PROJKTU
+
+#Analiza danych tekstowych powinna wykorzystywać przynajmniej jedną z omawianych technik:
+#np. analiza częstościsłów, analiza sentymentu, klastrowanie, itp.
+
+#Użytkownik powinien mieć możliwość uruchomienia analizy na dostarczonych danych (plikach).
+
+# W projekcie powinny znaleźć się elementy wizualizacji wyników (np. wykresy ggplot2, chmury słów).
+# Kod musi być czytelnie udokumentowany (komentarze, podział na sekcje).
